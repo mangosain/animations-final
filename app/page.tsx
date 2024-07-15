304636bed22b209d7ead28182424f3d5c7470dd3
Manav Gosain <mu@Manavs-MacBook-Pro.local>
2024-07-15 20:20:56 +0530

    added a type to time

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
 
-    function raf(time) {
+    function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
